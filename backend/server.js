488ad5ef2410e3620f4c52002b7a5f40a7a2f054
doomwave <acunanajair9@gmail.com>
2025-06-11 15:32:02 -0700

    NotesRoutes ready and organized the structure.

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,11 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes"
 
 const app = express();
 
+app.use("/api/notes", notesRoutes);
 
 
-
-app.get("/api/notes", (req,res) => {
-    res.status(200).send("you got 5 notes");
-});
-
-app.post("api/notes", (req,res) => {
-    res.status(201).json({message: "Note created successfully"})
-});
-
-app.put("api/notes:id", (req,res) => {
-    res.status(200).json({message: "Note updated successfully"})
-});
-
-app.delete("api/notes:id", (req,res) => {
-    res.status(200).json({message: "Note deleted successfully"})
-});
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
