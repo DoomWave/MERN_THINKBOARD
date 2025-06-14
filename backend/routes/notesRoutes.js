1a15d1598af1452b2f086c82c6fba73927b55168
doomwave <acunanajair9@gmail.com>
2025-06-13 21:37:48 -0700

    noteRoutes optimize to notesController

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,21 +1,12 @@
 import express from "express"
-import { getAllNotes } from "../controllers/notesController";
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
-
-router.post("/", (req,res) => {
-    res.status(201).send("Note created successfully!");
-});
-
-router.put("/:id", (req,res) => {
-    res.status(200).json({message: "Note updated successfully"})
-});
-
-router.delete("/:id", (req,res) => {
-    res.status(200).json({message: "Note deleted successfully"})
-});
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router
 
