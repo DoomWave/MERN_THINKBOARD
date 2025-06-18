ee6ae5ba9e8ebe7ae6a977692df9cad9311433fd
doomwave <acunanajair9@gmail.com>
2025-06-18 11:24:06 -0700

    notification by using Toast library works.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,20 @@
+import { Route, Routes } from "react-router";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
+
 const App = () => {
   return (
-    <div></div>
-  )
-}
-export default App
+    <div>
+      <button onClick={()=> toast.success("congrats!")}>Click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
